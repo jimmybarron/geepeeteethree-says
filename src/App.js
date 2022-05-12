9b345ff40ac403fdc0a151574c014f56e4b26813
Jimmy Barron <jimmy.r.barron@gmail.com>
2022-05-11 17:00:42 -0700

    Created SubmitForm Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
-import Button from "./Button";
-import submitOpenAi from "./submitOpenAi";
 import "./App.css";
 import RespList from "./RespList";
+import SubmitForm from "./SubmitForm";
 
 // ✅ Trigger 'loading' state before response and clear after response is received
 // ✅ Check if response is 'ok' and throw error if not
@@ -11,51 +10,18 @@ function App() {
   const [aiResp, setAiResp] = useState([]);
   const [aiPrompt, setAiPrompt] = useState();
 
-  // useEffect(() => {
-  //   console.log(aiResp);
-  // }, [aiResp]);
-
   return (
     <>
       <header>
         <h1 style={{ textAlign: "center" }}>Open AI</h1>
       </header>
       <main id="main" className="App">
-        <section>
-          <form
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-            onSubmit={(event) => {
-              event.preventDefault();
-            }}
-          >
-            <label htmlFor="aiPrompt">Ask the computer a question:</label>
-            <textarea
-              id="aiPrompt"
-              style={{ width: "70vw", height: "10vh", fontSize: "24px" }}
-              value={aiPrompt}
-              onChange={(event) => {
-                setAiPrompt(event.target.value);
-              }}
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  submitOpenAi(event, aiPrompt, setAiPrompt, aiResp, setAiResp);
-                }
-              }}
-            ></textarea>
-            <Button
-              onClick={(event) =>
-                submitOpenAi(event, aiPrompt, setAiPrompt, setAiResp)
-              }
-            >
-              Submit to Ai
-            </Button>
-          </form>
-        </section>
-
+        <SubmitForm
+          aiPrompt={aiPrompt}
+          setAiPrompt={setAiPrompt}
+          aiResp={aiResp}
+          setAiResp={setAiResp}
+        />
         <RespList aiResp={aiResp} />
       </main>
     </>
---
+++ src/SubmitForm.js
@@ -0,0 +1,43 @@
+import submitOpenAi from "./submitOpenAi";
+import Button from "./Button";
+
+const SubmitForm = ({ aiPrompt, setAiPrompt, aiResp, setAiResp }) => {
+  return (
+    <section>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label htmlFor="aiPrompt">Ask the computer a question:</label>
+        <textarea
+          id="aiPrompt"
+          style={{ width: "70vw", height: "10vh", fontSize: "24px" }}
+          value={aiPrompt}
+          onChange={(event) => {
+            setAiPrompt(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              submitOpenAi(event, aiPrompt, setAiPrompt, aiResp, setAiResp);
+            }
+          }}
+        ></textarea>
+        <Button
+          onClick={(event) =>
+            submitOpenAi(event, aiPrompt, setAiPrompt, setAiResp)
+          }
+        >
+          Submit to Ai
+        </Button>
+      </form>
+    </section>
+  );
+};
+
+export default SubmitForm;
