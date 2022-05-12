916fc61a6f633d1844b5486e764d5abe337fbf00
Jimmy Barron <jimmy.r.barron@gmail.com>
2022-05-12 14:05:51 -0700

    Added serverless func; returning html

--- src/SubmitForm.js
+++ src/SubmitForm.js
@@ -1,7 +1,33 @@
-import submitOpenAi from "./submitOpenAi";
 import Button from "./Button";
 
 const SubmitForm = ({ aiPrompt, setAiPrompt, aiResp, setAiResp }) => {
+  const submitOpenAi = async (
+    event,
+    aiPrompt,
+    setAiPrompt,
+    aiResp,
+    setAiResp
+  ) => {
+    // This calls the serverless function
+    const respOpenAi = await fetch("./netlify/functions/queryOpenAi").then(
+      (response) => response
+    );
+    const aiRespText = await respOpenAi.text();
+    console.log(aiRespText);
+
+    setAiResp((prevState) => {
+      return [
+        ...prevState,
+        {
+          aiPrompt: aiPrompt,
+          // aiResp: aiRespText,
+        },
+      ];
+    });
+
+    setAiPrompt("");
+  };
+
   return (
     <section>
       <form
@@ -30,7 +56,7 @@ const SubmitForm = ({ aiPrompt, setAiPrompt, aiResp, setAiResp }) => {
         ></textarea>
         <Button
           onClick={(event) =>
-            submitOpenAi(event, aiPrompt, setAiPrompt, setAiResp)
+            submitOpenAi(event, aiPrompt, setAiPrompt, aiResp, setAiResp)
           }
         >
           Submit to Ai
